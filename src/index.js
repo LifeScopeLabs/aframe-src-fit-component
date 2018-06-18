758bd3d5079e536bedfe69c8925f75598842cf2f
mohrtw <mohrtw@gmail.com>
2018-06-18 16:43:24 -0700

    added orientation option

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ if (typeof AFRAME === 'undefined') {
   throw new Error('Component attempted to register before AFRAME was available.');
 }
 
-console.log("AFRAME.registerComponent('src-fit', ...");
 AFRAME.registerComponent('src-fit', {
     dependencies: ['geometry', 'material'],
+
+    schema: {
+        orientation: {default: 'auto', oneOf: ['auto', 'width', 'height']}
+      },
 
     fit: function (w, h) {
         var ratio = (h || 1.0) / (w || 1.0);
@@ -14,16 +17,21 @@ AFRAME.registerComponent('src-fit', {
         var neww, newh;
         // W
         if (geo && geo.data.width) {
+            console.log('W');
             // W < H
             if (geo && geo.data.height && ratio > 1) {
+                console.log('W < H');
                 neww = geo.data.width / ratio;
             } else {  // W !H || W > H
+                console.log('W !H || W > H');
                 newh = geo.data.width * ratio;
             }
         } else {  // !W H
             if (geo && geo.data.height) {
+                console.log('!W H');
                 neww = geo.data.height / ratio;
             } else { // !W !H
+                console.log('!W !H');
                 // variable width and height, stay smaller than 1
                 neww = Math.min(1.0, 1.0 / ratio);
                 newh = Math.min(1.0, ratio);
@@ -34,12 +42,60 @@ AFRAME.registerComponent('src-fit', {
         this.el.emit('fit', [neww, newh]);
     },
 
+    fitWidth: function (w, h) {
+        var ratio = (h || 1.0) / (w || 1.0);
+        var geo = this.el.components.geometry;
+        var newh;
+        // W
+        if (geo && geo.data.width) {
+            console.log('W');
+            newh = geo.data.width * ratio;
+            
+        } else {  // !W H
+            console.log('fitWidth requires geometry.data.width to be set.');
+        }
+        if (newh !== undefined) { this.el.updateComponent('geometry', {height: newh});}
+        this.el.emit('fit', [geo.data.width, newh]);
+    },
+
+    fitHeight: function (w, h) {
+        var ratio = (h || 1.0) / (w || 1.0);
+        var geo = this.el.components.geometry;
+        var neww;
+        // W
+        if (geo && geo.data.height) {
+            console.log('H');
+            neww = geo.data.width / ratio;
+            
+        } else {  // !W H
+            console.log('fitHeight requires geometry.data.height to be set.');
+        }
+        if (neww !== undefined) { this.el.updateComponent('geometry', {width: neww});}
+        this.el.emit('fit', [neww, geo.data.height]);
+    },
+
     onMaterialLoaded: function (e) {
         var self = this;
         var src = e.detail.src;
+        console.log("onMaterialLoaded");
+        console.log(src);
         var w = src.videoWidth || src.width;
         var h = src.videoHeight || src.height;
-        if (w || h) { self.fit(w, h); }
+        console.log("w: " + w);
+        console.log("h: " + h);
+        //if (w || h) { self.fit(w, h); }
+        if (this.data.orientation == 'auto' && (w || h)) {
+            console.log("orientation: auto");
+            self.fit(w, h);
+        }
+        else if (this.data.orientation == 'width') {
+            console.log("orientation: width");
+            self.fitWidth(w, h);
+        }
+        else if (this.data.orientation == 'height') {
+            console.log("orientation: height");
+            self.fitHeight(w, h);
+        }
     },
 
     init: function () {
